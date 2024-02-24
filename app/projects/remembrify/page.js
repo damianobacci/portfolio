f708334ae2e20295986c936592ab5d60b95d3210
Damiano Bacci <damiano.bacci@gmail.com>
2024-02-24 13:11:54 +0100

    Remembrify page

--- app/projects/remembrify/page.js
+++ app/projects/remembrify/page.js
@@ -3,7 +3,8 @@ import ProjectFooter from "@/components/UI/ProjectFooter";
 import OutlineButton from "@/components/UI/OutlineButton";
 // import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 // import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import Image from "next/image";
+
+import ProjectImage from "@/components/UI/ProjectImage";
 
 export default function Remembrify() {
   // const code =
@@ -20,47 +21,36 @@ export default function Remembrify() {
       <OutlineButton icon="live" link="">
         View Live
       </OutlineButton>
-      <Image
-        src="/images/projects/remembrify.jpg"
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{
-          width: "100%",
-          height: "auto",
-          marginBottom: "0.5em",
-          marginTop: "1em",
-        }}
-      />
+      <ProjectImage alt="Remembrify" link="/images/projects/remembrify.jpg" />
       <h2 className="projectTitle">Project Purpose and Goal</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus cum
-        nisi quos vero reiciendis sint nihil quod, cupiditate autem, iure animi
-        qui illo accusamus accusantium, voluptatibus magni ad architecto. Iusto?
+      <p className="projectParagraph">
+        While working at my desk, I am sometimes completely absorbed by the
+        subject I am working on, browsing multiple StackOverflow discussions,
+        trying different code snippets, testing, reading.
       </p>
-      <Image
-        src="/images/projects/remembrify.jpg"
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{
-          width: "100%",
-          height: "auto",
-          marginBottom: "0.5em",
-          marginTop: "1em",
-        }}
-      />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus cum
-        nisi quos vero reiciendis sint nihil quod, cupiditate autem, iure animi
-        qui illo accusamus accusantium, voluptatibus magni ad architecto. Iusto?
+
+      <p className="projectParagraph">
+        But for the sake of your own health, there are some things you cannot
+        forget to do: experts recommend getting up from your chair{" "}
+        <a href="https://hr.umich.edu/benefits-wellness/health-well-being/mhealthy/faculty-staff-well-being/physical-activity/time-move#:~:text=Remember%20to%20move%20for%20approximately,helps%20us%20feel%20our%20best.">
+          for approximately three minutes every 30-60 minutes
+        </a>
+        , <a>drinking water every one hour</a>, and in my case where I wear
+        contact lenses, <a> keep my eyes hydrated every hour</a>.
+      </p>
+      <p className="projectParagraph">
+        I tried to set alarms on my phone, but I ended up getting distracted by
+        messages and notifications. How can I have a simple system of reminders
+        that can be personalized on your needs?
       </p>
       <h2 className="projectTitle">Problems and Thought Process</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus cum
-        nisi quos vero reiciendis sint nihil quod, cupiditate autem, iure animi
-        qui illo accusamus accusantium, voluptatibus magni ad architecto. Iusto?
+      <p className="projectParagraph">
+        I needed a set of countdowns very easy to set up, that could be added or
+        deleted on the fly, and that could be controlled individually or
+        together.
       </p>
+      <ProjectImage alt="Remembrify" link="/images/projects/remembrify2.jpg" />
+      <p className="projectParagraph">But</p>
       {/* <SyntaxHighlighter
         style={atomDark}
         language="javascript"
--- components/UI/ProjectImage.js
+++ components/UI/ProjectImage.js
@@ -1,17 +1,10 @@
+import styles from "./ProjectImage.module.css";
 import Image from "next/image";
 
-export default function ProjectImage(props) {
-  <Image
-    src={props.link}
-    alt="Testo"
-    width={0}
-    height={0}
-    sizes="100vw"
-    style={{
-      width: "100%",
-      height: "auto",
-      marginBottom: "0.5em",
-      marginTop: "1em",
-    }}
-  />;
+export default function ProjectImage({ link, alt }) {
+  return (
+    <div className={styles.imageWrapper}>
+      <Image src={link} alt={alt} width={0} height={0} sizes="100vw" />
+    </div>
+  );
 }
