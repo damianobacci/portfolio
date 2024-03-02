148223a10e4718d21da1045af6dcc7c88505a914
Damiano Bacci <damiano.bacci@gmail.com>
2024-03-02 17:31:06 +0100

    New about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,13 +1,15 @@
 import JobBox from "@/components/UI/JobBox";
-import Link from "next/link";
+import OutlineButton from "@/components/UI/OutlineButton";
 
 export default function AboutPage() {
   return (
     <main>
       <h1 className="title">ABOUT</h1>
-      <p>This is my story.</p>
-      <button>Download CV</button>
-      <h2>Full professional experience</h2>
+      <p className="projectParagraph">This is my story.</p>
+      <OutlineButton icon="download" link="https://city-ears.vercel.app/">
+        Download Resume
+      </OutlineButton>
+      {/* <h2>Full professional experience</h2>
       <JobBox
         date="Oct 2022 - Jun 2023"
         link="https://www.acaps.org/"
@@ -22,6 +24,13 @@ export default function AboutPage() {
         job="Data/Content Journalist"
         description="Asd"
       />
+      <JobBox
+        date="Nov 2018 - Sep 2022"
+        link="https://www.datatalk.it/"
+        place="Dalk"
+        job="Data/Content Journalist"
+        description="Asd"
+      /> */}
     </main>
   );
 }
--- components/UI/OutlineButton.js
+++ components/UI/OutlineButton.js
@@ -1,6 +1,6 @@
 import styles from "./OutlineButton.module.css";
 import Link from "next/link";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaCloudDownloadAlt } from "react-icons/fa";
 import { MdReadMore } from "react-icons/md";
 import { LuExternalLink } from "react-icons/lu";
 
@@ -12,6 +12,7 @@ export default function OutlineButton({ icon, children, link }) {
         {icon == "code" && <FaGithub className={styles.icon} />}
         {icon == "more" && <MdReadMore className={styles.icon} />}
         {icon == "live" && <LuExternalLink className={styles.icon} />}
+        {icon == "download" && <FaCloudDownloadAlt className={styles.icon} />}
       </button>
     </Link>
   );
